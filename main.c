e9b6bffb2eefa36895d04d68d1b4ca120bce4ff9
linabigail <alin50@stuy.edu>
2024-11-25 21:52:28 -0500

    spent a while trying to find out why i got negative numbers, found out it was because i was not using unsigned int. finished all child process printing

--- main.c
+++ main.c
@@ -5,19 +5,43 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
-int main(){
-  pid_t child1 = fork();
-  pid_t child2 = fork();
-  if (child1 < 0 || child2 < 0){
-    perror("fork failed.\n");
+int getran(){
+  int fd = open("/dev/urandom", O_RDONLY);
+  if (fd < 0) {
+    perror("/dev/urandom failed.");
     exit(1);
   }
-  int fd = open("/dev/urandom", O_RDONLY);
-  int rand;
+  unsigned int rand;
   read(fd,&rand,sizeof(rand));
-  rand = rand % 5 + 1;
-  printf("PID: %d Randon Number: %d\n",getpid(),rand);
-  sleep(rand);
-  printf("");
+  return rand % 5 + 1;
+}
+
+int main(){
+  pid_t child1 = fork();
+  if (child1 < 0){
+    perror("fork failed.");
+    exit(1);
+  }
+  if (child1 == 0){
+    int rand = getran();
+    printf("PID: %d Randon Number: %d\n",getpid(),rand);
+    sleep(rand);
+    printf("%d finished after %d seconds\n",getpid(),rand);
+    exit(0);
+  }
+  pid_t child2 = fork();
+  if (child2 < 0){
+    perror("fork failed.");
+    exit(1);
+  }
+  if (child2 == 0){
+    int rand = getran();
+    printf("PID: %d Randon Number: %d\n",getpid(),rand);
+    sleep(rand);
+    printf("%d finished after %d seconds\n",getpid(),rand);
+    exit(0);
+  }
+  wait(NULL);
+  wait(NULL);
   return 0;
 }
